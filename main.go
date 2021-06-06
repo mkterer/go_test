9c453e3e33477283c2b1ba0e148704de76370c90
mkterer <>
2021-06-06 22:57:46 +0900

    ミドルウェア追加

--- main.go
+++ main.go
@@ -10,11 +10,18 @@ func main() {
 	engine := gin.Default()
 	// htmlのディレクトリを指定
 	engine.Static("/static", "./static")
+	ua := ""
+	// ミドルウェアを使用
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
 	engine.LoadHTMLGlob("templates/*")
 	engine.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			// htmlに渡す変数を定義
 			"message": "hello gin",
+			"aaaaa":   ua,
 		})
 	})
 	engine.Run(":3000")
